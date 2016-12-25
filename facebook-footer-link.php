f00cdad9874edfb7cbd346d68f076d4bd2b14a5b
Daniel Cortes <renaissance.scholar2012@gmail.com>
2016-12-25 01:39:04 +0000

    Developed Admin Settings

--- facebook-footer-link.php
+++ facebook-footer-link.php
@@ -12,7 +12,16 @@ if(!defined('ABSPATH')){
 	exit;
 }
 
+// Global Options Variable
+$ffl_options = get_option('ffl_settings');
+
 // Load Scripts
 require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-scripts.php');
 
+// Load Content
 require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-content.php');
+
+if(is_admin()){
+	// Load Settings
+	require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-settings.php');
+}
